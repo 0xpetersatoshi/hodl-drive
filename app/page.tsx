9289dfce3260af8857912dea1f9e9ac99e73473d
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2023-08-23 12:58:19 -0700

    feat: integrate rainbow kit into app UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 import UploadForm from "./components/upload-form/upload-form.component";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
-      <UploadForm />
+      <div className="mb-12">
+        <ConnectButton />
+      </div>
+      <div>
+        <UploadForm />
+      </div>
     </main>
   );
 }
---
+++ app/providers.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React from "react";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, base, zora } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, publicClient } = configureChains(
+  [mainnet, polygon, optimism, arbitrum, base, zora],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_NAME as string,
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID as string,
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => setMounted(true), []);
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains} appInfo={{ appName: "HODL Drive" }}>
+        {mounted && children}
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+};
