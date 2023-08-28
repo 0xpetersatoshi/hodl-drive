82f51de433b50171cb89090168a826a5f03311d2
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2023-08-28 11:39:41 -0700

    chore: add svg logo

--- app/components/navigation/navigation.component.tsx
+++ app/components/navigation/navigation.component.tsx
@@ -1,11 +1,20 @@
 import Link from "next/link";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center bg-custom-navbar-color p-4">
       <Link href="/" className="text-xl font-bold text-white">
-        HODL Drive
+        <div className="flex items-center text-xl font-bold text-white">
+          <Image
+            src="/hodl-drive.svg"
+            alt="HODL Drive Logo"
+            width={48}
+            height={48}
+          />
+          HODL Drive
+        </div>
       </Link>
 
       <ul className="flex space-x-4 items-center">
