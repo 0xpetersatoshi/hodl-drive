4fb4515775ecfd59bac2e491c1e3ab769a2ec733
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2023-08-28 09:49:28 -0700

    chore: change navbar color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       backgroundColor: {
-        "custom-navbar-color": "#512B81",
+        "custom-navbar-color": "#4477CE",
       },
     },
   },
